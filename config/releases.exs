674c010f880570ec322f027a52f3a472171083c6
Orange <904412343@qq.com>
2019-11-28 21:57:20 +0800

    Watch excel files

--- config/releases.exs
+++ config/releases.exs
@@ -1,14 +1,15 @@
 import Config
 
 # path mapping between local and remote project dir
-source_dir = "c:/Users/wenjuxu/Desktop/svr_proj/trunk"
-remote_dir = "/data1/home/wenjuxu/svr_proj/trunk"
+source_dir = "c:/Users/wenjuxu/Desktop/KOServer"
+remote_dir = "/data1/home/wenjuxu/KOServer"
 
 watch_files =
   [
     # {dir, {file_type, file_regex}}
     {Path.join(source_dir, "libsrc/protocol"), {:protocol, ~r/.*\.proto$/}},
-    {Path.join(source_dir, "libsrc/log"), {:oss_desc, ~r/^oss_desc\.xml$/}}
+    {Path.join(source_dir, "libsrc/log"), {:oss_desc, ~r/^oss_desc\.xml$/}},
+    {Path.join(source_dir, "excel/excel_config"), {:excel, ~r/^[^~].*\.xlsx$/}}
   ]
   |> Enum.into(%{})
 
@@ -17,7 +18,8 @@ remote_addr = "http://10.125.40.202:8759"
 upload_urls = [
   # file_type: upload_url
   protocol: URI.merge(remote_addr, "/upload/protocol") |> to_string(),
-  oss_desc: URI.merge(remote_addr, "/upload/oss_desc") |> to_string()
+  oss_desc: URI.merge(remote_addr, "/upload/oss_desc") |> to_string(),
+  excel: URI.merge(remote_addr, "/upload/excel") |> to_string()
 ]
 
 http_proxy = {:http, "web-proxy.tencent.com", 8080}
